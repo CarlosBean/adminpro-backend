4416fe6d51d21f9e165f04850ba5121b689a065c
Carlos Benavides <1992cab@gmail.com>
2019-08-18 22:46:36 -0500

    chore: added user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const moongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+let mongooseHidden = require('mongoose-hidden')()
+
+const Schema = moongoose.Schema;
+
+const acceptedRoles = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} is not allowed'
+};
+
+const userSchema = new Schema({
+    name: { type: String, required: [true, 'name is required'] },
+    email: { type: String, unique: true, required: [true, 'email is required'] },
+    password: { type: String, required: [true, 'password is required'] },
+    img: { type: String, required: false },
+    role: { type: String, required: false, default: 'USER_ROLE', enum: acceptedRoles }
+});
+
+userSchema.plugin(uniqueValidator, { message: '{PATH} must be unique' });
+userSchema.plugin(mongooseHidden, { hidden: { _id: false, password: true } });
+
+module.exports = moongoose.model('user', userSchema);

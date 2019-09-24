991387ede2160f489cfdd0e453a1357b21fa85da
Carlos Benavides <1992cab@gmail.com>
2019-09-23 23:24:21 -0500

    chore: get token from header

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,14 @@ const jwt = require('jsonwebtoken');
 const SEED = require('../config/config').SEED;
 
 exports.verifyToken = function (req, res, next) {
-    const token = req.query.token;
+    // const token = req.query.token;
+    const authHeader = req.header('Authorization');
+    let token = '';
+
+    if (authHeader) {
+        token = authHeader.split(' ')[1];
+    }
+
     jwt.verify(token, SEED, (err, decoded) => {
         if (err) {
             return res.status(401).json({
@@ -15,4 +22,35 @@ exports.verifyToken = function (req, res, next) {
         req.currentUser = decoded.user;
         next();
     })
+}
+
+exports.verifyAdmin = function (req, res, next) {
+
+    const user = req.currentUser;
+
+    if (user.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: 'unauthorized access',
+            errors: { message: 'must be admin for this operation' }
+        });
+    }
+}
+
+exports.verifyAdmin_or_sameUser = function (req, res, next) {
+
+    const user = req.currentUser;
+    const id = req.params.id;
+
+    if (user.role === 'ADMIN_ROLE' || user._id === id) {
+        next();
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: 'unauthorized access',
+            errors: { message: 'must be admin or owner info for this operation' }
+        });
+    }
 }
